e9ad74db4a733ed43a44e26036af62cd8388e743
Patrícia Gomes <patriciagomes@airdepatricia3.lan>
2023-12-06 18:00:19 +0000

    gymflex

--- sql/gymflex.sql
+++ sql/gymflex.sql
@@ -119,12 +119,12 @@ CREATE TABLE Plano (
 );
 
 -- Tabela Aulagrupo
-CREATE TABLE Aulagrupo (
+/* CREATE TABLE Aulagrupo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     qntd_membros INTEGER NOT NULL,
     ginasio INTEGER NOT NULL,
     tipo_ag INTEGER NOT NULL,
-    FOREIGN KEY (tipo_ag) REFERENCES Tipo_ag(id),
+    FOREIGN KEY (tipo_ag) REFERENCES Tipo_ag(nome),
     FOREIGN KEY (ginasio) REFERENCES Ginasio(id),
     CHECK (qntd_membros > 0)
     --CHECK (qntd_membros <= capacidade FROM Tipo_ag WHERE tipo_ag = Aulagrupo.tipo_ag) 
@@ -133,8 +133,7 @@ CREATE TABLE Aulagrupo (
 
 
 CREATE TABLE Tipo_ag(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome VARCHAR(255) NOT NULL UNIQUE,
+    nome VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
     capacidade INT NOT NULL,
     dia_semana VARCHAR(255) NOT NULL,
     hora_inicio TIME NOT NULL,
@@ -145,8 +144,39 @@ CREATE TABLE Tipo_ag(
     CHECK (capacidade > 0),
     imagem_aulagrupo VARCHAR(255)
 );
+ */
 
+CREATE TABLE Aulagrupo (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    data DATE,
+    hora_inicio TIME NOT NULL,
+    hora_fim TIME,
+    duracao_ag TIME,
+    capacidade INT NOT NULL,
+    pt1 INTEGER NOT NULL,
+    pt2 INTEGER NOT NULL,
+    ginasio INTEGER NOT NULL,
+    FOREIGN KEY (pt1) REFERENCES Personaltrainer(id),
+    FOREIGN KEY (pt2) REFERENCES Personaltrainer(id),
+    FOREIGN KEY (ginasio) REFERENCES Ginasio(id),
+    CHECK (data IS NOT NULL),
+    CHECK (hora_fim IS NULL OR hora_fim > hora_inicio),
+    CHECK (duracao_ag = strftime('%s', hora_fim) - strftime('%s', hora_inicio)),
+    CHECK (capacidade > 0)
+);
 
+-- Tabela Inscricao_ag
+CREATE TABLE Inscricao_ag (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    qntd_membros INTEGER NOT NULL,
+    membro INTEGER NOT NULL,
+    aulagrupo INTEGER NOT NULL,
+    FOREIGN KEY (membro) REFERENCES Membro(id),
+    FOREIGN KEY (aulagrupo) REFERENCES Aulagrupo(id),
+    CHECK (qntd_membros >= 0),
+    CHECK (qntd_membros <= (SELECT capacidade FROM Aulagrupo WHERE Aulagrupo.id = Inscricao_ag.aulagrupo))
+);
 -- php: qntd_membros corresponde à soma de clientes que se inscreveram numa certa Aulagrupo
 
 
@@ -211,8 +241,27 @@ VALUES
   ('2020-01-02', 2, 'Intermédio'),
   ('2020-01-03', 3, 'Avançado');
 
+-- Inserir dados na tabela Tipo_ag
+INSERT INTO Tipo_ag (nome, capacidade, dia_semana, hora_inicio, hora_fim, duracao_ag, imagem_aulagrupo)
+VALUES 
+  ('Zumba', 18, 'Sexta-Feira', '17:30', '18:30', strftime('%s', '18:30') - strftime('%s', '17:30'), 'imagens/zumba.jpeg'),
+  ('Cycling', 15, 'Segunda-Feira', '10:00', '11:30', strftime('%s', '11:30') - strftime('%s', '10:00'), 'imagens/cycling.jpeg'),
+  ('Pilates', 10, 'Terça-Feira', '10:00', '11:30', strftime('%s', '11:30') - strftime('%s', '10:00'), 'imagens/pilates.jpeg'),
+  ('Xpress Abs', 15, 'Sábado', '17:30', '18:30', strftime('%s', '18:30') - strftime('%s', '17:30'), 'imagens/xpressabs.jpeg'),
+  ('Body Pump', 18, 'Quinta-Feira', '17:30', '18:30', strftime('%s', '18:30') - strftime('%s', '17:30'), 'imagens/bodypump.jpeg'),
+  ('Body Step', 15, 'Quarta-Feira', '14:30', '15:30', strftime('%s', '15:30') - strftime('%s', '14:30'), 'imagens/bodystep.jpeg');
 
+-- Inserir dados na tabela Aulagrupo
 INSERT INTO Aulagrupo (qntd_membros, ginasio, tipo_ag, imagem_ag)
+VALUES 
+  (0, 1, 'Zumba', 'imagens/zumba.jpeg'),
+  (0, 1, 'Cycling', 'imagens/cycling.jpeg'),
+  (0, 1, 'Pilates', 'imagens/pilates.jpeg'),
+  (0, 1, 'Xpress Abs', 'imagens/xpressabs.jpeg'),
+  (0, 1, 'Body Pump', 'imagens/bodypump.jpeg'),
+  (0, 1, 'Body Step', 'imagens/bodystep.jpeg');
+
+/* INSERT INTO Aulagrupo (qntd_membros, ginasio, tipo_ag, imagem_ag)
 VALUES 
   (0, 1, 'Zumba','imagens/zumba.jpeg'),
   (0, 1, 'Cycling','imagens/cycling.jpeg'),
@@ -231,13 +280,4 @@ VALUES
   /*('Yoga', 20, 'Sexta-Feira', '16:00','17:30'),*/
   ('Xpress Abs', 15,'Sábado', '17:30', '18:30','imagens/xpressabs.jpeg');
 
-
-
-INSERT INTO Aulagrupo (qntd_membros, ginasio, tipo_ag, imagem_ag)
-VALUES 
-  (0, 1, 'Zumba','imagens/zumba.jpeg'),
-  (0, 1, 'Cycling','imagens/cycling.jpeg'),
-  (0, 1, 'Pilates', 'imagens/pilates.jpeg'),
-  (0, 1,'Xpress Abs','imagens/xpressabs.jpeg'),
-  (0, 1, 'Body Pump', 'imagens/bodypump.jpeg'),
-  (0, 1, 'Body Step','imagens/bodystep.jpeg');
+ */
