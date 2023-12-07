92cc52dc96c80793f71324ee0b5e7e1825f4402a
Marta M. Monteiro <martam.monteiro@Air-de-Marta-5.Home>
2023-12-07 11:43:14 +0000

    area_cliente

--- area_cliente.php
+++ area_cliente.php
@@ -69,7 +69,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GymFlex</title>
     <link rel="icon" href="imagens/gymflex_logo_head.svg">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="estetica.css">
 </head>
 
 <body>
