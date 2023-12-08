aa4484a879fc47b10355123ef869595436e897f9
Marta M. Monteiro <martam.monteiro@Air-de-Marta-5.Home>
2023-12-08 18:25:53 +0000

    action_cancelamento

--- action_cancelamento.php
+++ action_cancelamento.php
@@ -2,21 +2,21 @@
 require_once("init.php");
 session_start();
 
-    try {
-        // Delete user info from Pessoa and related tables
-        $stmt = $dbh->prepare('DELETE FROM Pessoa WHERE email = ?');
-        $stmt->execute([$email]);
+try {
+    // Para obter o endereço de e-mail do formulário ou de onde quer que você esteja recebendo
+    $email = $_POST['email']; 
 
-        
+    // Delete user info from Pessoa and related tables
+    $stmt = $dbh->prepare('DELETE FROM Pessoa WHERE email = ?');
+    $stmt->execute([$email]);
 
-        // After cancellation, destroy the session and redirect to a confirmation page or login page
-        session_destroy();
-        header('Location: cancelamento.php'); // Redirect to confirmation page
-        exit();
+    // After cancellation, destroy the session and redirect to a confirmation page or login page
+    session_destroy();
+    header('Location: cancelamento.php'); // Redirect to confirmation page
+    exit();
 
-    } catch (PDOException $e) {
-        // Handle any errors that might occur during deletion
-        echo "Error: " . $e->getMessage();
-    }
-
+} catch (PDOException $e) {
+    // Handle any errors that might occur during deletion
+    echo "Error: " . $e->getMessage();
+}
 ?>
