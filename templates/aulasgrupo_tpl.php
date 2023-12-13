402306e55dfeb3f31daed9440b94339be74e90c3
Patrícia Gomes <patriciagomes@airdepatricia3.lan>
2023-12-13 21:56:19 +0000

    aulasgrupo_tpl

--- templates/aulasgrupo_tpl.php
+++ templates/aulasgrupo_tpl.php
@@ -24,5 +24,5 @@
 
 <div class="mensagem">
     <p>Inscreve-te já como membro para puderes usufruir destas aulas</p>
-    <a href="registo.php" class="button"> Inscreve-te aqui! </a>
+    <a href="registo.php" class="inscreva-se"> Inscreve-te aqui! </a>
 </div>
