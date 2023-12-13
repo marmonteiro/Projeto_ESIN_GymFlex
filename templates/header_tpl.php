b0754d1978be9130706ea106a66623d81423344e
ritita02 <rita.marques2002@gmail.com>
2023-12-13 21:21:28 +0000

    css header

--- templates/header_tpl.php
+++ templates/header_tpl.php
@@ -14,24 +14,31 @@
 <body>
     <header>
         <a href="paginicial.php">
-            <img id="logo" src="imagens/gymflex_logo.svg" alt="Logotipo">
+            <img id="logo" src="imagens/logo.gif" alt="Logotipo">
         </a>
-        <h1>GymFlex: Diferentes clubes em diferentes cidades.</h1>
-        <h2>Escolha a cidade mais perto de si e venha treinar connosco.</h2>
+        
+        <!-- <h1>GymFlex: Diferentes clubes em diferentes cidades.</h1>
+        <h2>Escolha a cidade mais perto de si e venha treinar connosco.</h2> -->
 
-        <div class="barra">
+        <div class="navegacao">
             <a href="clubes.php" class="clubes">Clubes</a>
             <a href="planos.php" class="planos">Planos</a>
             <a href="aulasgrupo.php" class="info">Aulas de Grupo</a>
             <a href="ajuda.php" class="ajuda">Ajuda</a>
         </div>
 
-        <?php if (isset($_SESSION['email'])) { ?>
-            <a href="action_logout.php" class="button">Logout</a>
-            <a href="area_cliente.php" class="button">Área de Cliente</a>
-        <?php } else { ?>
-            <a href="registo.php" class="inscreva-se">Inscreva-se</a>
-            <a href="login.php" id="signup">Login: área de cliente</a>
-        <?php } ?>
+        <div class="cliente">
+        <?php 
+            if (isset($_SESSION['email'])) { ?>  <!-- se sessão iniciada -->
+                <a href="action_logout.php" class="button"><img src = imagens/logout.png>Logout</a>
+                <a href="area_cliente.php" class="button"><img src = imagens/area_cliente.png>Área de Cliente</a>
+                <p>Olá,
+                    <?php echo $_SESSION['nome'] ?>!
+                </p>
+            <?php } else { ?> <!-- se sessão não iniciada -->
+                <a href="registo.php" class="inscreva-se"><img src = imagens/area_cliente.png>Inscreva-se</a>
+                <a href="login.php" id="signup"><img src = imagens/area_cliente.png>Login</a>
+            <?php } ?>
 
+        </div>
     </header>
--- templates/headerpaginicial_tpl.php
+++ templates/headerpaginicial_tpl.php
@@ -17,51 +17,38 @@
     <header class="header_paginicial">
         <section class="grid">
             <div class="logo">
-                <img src="imagens/logo.gif" alt="GymFlex Logo">
+                <img id="logo" src="imagens/gymflex_logo.svg" alt="Logotipo">
             </div>
             <div class="Torne-seMembro">
                 <p><a href="registo.php" class="inscreva-se">Torne-se membro!</a></p>
                 <p id=jámembro>Já é membro? Inicie sessão <a href="login.php">aqui.</a></p>
             </div>
         </section>
-        
-            <div class="barra">
-                <a href="clubes.php" class="clubes">Clubes</a>
-                <a href="planos.php" class="planos">Planos</a>
-                <a href="aulasgrupo.php" class="info">Aulas de Grupo</a>
-                <a href="ajuda.php" class="ajuda">Ajuda</a>
-            </div>
-       
 
+        <div class="barra">
+            <a href="clubes.php" class="clubes">Clubes</a>
+            <a href="planos.php" class="planos">Planos</a>
+            <a href="aulasgrupo.php" class="info">Aulas de Grupo</a>
+            <a href="ajuda.php" class="ajuda">Ajuda</a>
+        </div>
+
+        <div>
+            <?php
+            if (isset($_SESSION['email'])) { ?> <!-- se sessão iniciada -->
+                <a href="action_logout.php" class="button">Logout</a>
+                <a href="area_cliente.php" class="button">Área de Cliente</a>
+                <p>Olá,
+                    <?php echo $_SESSION['nome'] ?>!
+                </p>
+            <?php } else { ?> <!-- se sessão não iniciada -->
+                <a href="registo.php" class="inscreva-se">Inscreva-se</a>
+                <a href="login.php" id="signup">Login: área de cliente</a>
+            <?php }
+
+            if (isset($_SESSION['msg']) && !empty($_SESSION['msg'])) { ?> <!-- se houver mensagem de erro -->
+                <p>{$_SESSION['msg']}</p>"
+                <?php unset($_SESSION['msg']);
+            } ?>
+
+        </div>
     </header>
-
-    <!--  <header>
-    <a href="paginicial.php">
-        <img id="logo" src="imagens/logo.gif" alt="Logotipo">
-    </a>
-    <h1>GymFlex: Diferentes clubes em diferentes cidades</h1>
-
-    <div class="barra">
-        <a href="clubes.php" class="clubes">Clubes</a>
-        <a href="planos.php" class="planos">Planos</a>
-        <a href="aulasgrupo.php" class="info">Aulas de Grupo</a>
-        <a href="ajuda.php" class="ajuda">Ajuda</a>
-    </div>
-
-    <?php if (isset($_SESSION['email'])) { ?>
-        <a href="action_logout.php" class="button">Logout</a>
-        <a href="area_cliente.php" class="button">Área de Cliente</a>
-        <p>Olá,
-            <?php echo $_SESSION['nome'] ?>!
-        </p>
-    <?php } else { ?>
-        <a href="registo.php" class="inscreva-se">Inscreva-se</a>
-        <a href="login.php" id="signup">Login: área de cliente</a>
-    <?php }
-
-    if (isset($_SESSION['msg']) && !empty($_SESSION['msg'])) {
-        echo "<p>{$_SESSION['msg']}</p>";
-        unset($_SESSION['msg']);
-    } ?>
-
-    </header> -->
