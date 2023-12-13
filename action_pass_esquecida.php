8efe39709945ea3ee186bb5b7d96975d4f59258c
Patrícia Gomes <patriciagomes@airdepatricia3.lan>
2023-12-13 14:40:45 +0000

    action_send_pass

--- action_pass_esquecida.php
+++ action_pass_esquecida.php
@@ -11,10 +11,10 @@ $to = $_POST["email"];
 $subject = getNamePersonByEmail($to);
 
 if ($subject === null OR !filter_var($to, FILTER_VALIDATE_EMAIL)){
-    header("location: pass-esquecida-falha_tpl.php");
+    header("location: templates/pass-esquecida-falha_tpl.php");
 }
 else {
 
-  header("location: pass-esquecida-sucesso_tpl.php");
+  header("location: templates/pass-esquecida-sucesso_tpl.php");
 }
 ?>
