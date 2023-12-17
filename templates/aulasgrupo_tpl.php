449661b69ab5c22dbabcd0d4111e74fbdbda1776
Patrícia Gomes <patriciagomes@AirdePatricia3.Home>
2023-12-17 17:08:08 +0000

    aulasgrupo

--- templates/aulasgrupo_tpl.php
+++ templates/aulasgrupo_tpl.php
@@ -1,25 +1,23 @@
 <div class="classes">
-    <?php
-    if (isset($aulas) && !empty($aulas)) {
-        foreach ($aulas as $aula) {
-            echo '<div class="class">';
-            echo '<div class="image-container">';
-            echo '<img src="' . $aula['imagem_ag'] .'" alt="' . $aula['nome'] . '">';
-            echo '</div>';
-            echo '<div class="info-container">';
-            echo '<p>' . $aula['nome'] . '</p>';
-            echo '<p> Capacidade: ' . $aula['capacidade'] . '</p>';
-            echo '<p>Dia da semana: ' . $aula['dia_semana'] . '</p>';
-            echo '<p>Horário: ' . $aula['hora_inicio'] . ' - ' . $aula['hora_fim'] . '</p>';
-            echo '</div>';
-            echo '</div>';
-        }
-    } else {
-        echo '<p>Não existem aulas de grupo disponíveis de momento.</p>';
-    }
-    ?>
+    
+<?php if (isset($aulas) && !empty($aulas)) : ?>
+    <?php foreach ($aulas as $aula) : ?>
+        <div class="class">
+            <div class="image-container">
+                <img src="<?php echo $aula['imagem_ag'] ?>" alt="<?php echo $aula['nome'] ?>">
+            </div>
+            <div class="info-container">
+                <p> <?php echo $aula['nome'] ?></p>
+                <p> Capacidade: <?php echo $aula['capacidade'] ?></p>
+                <p> Dia da semana: <?php echo $aula['dia_semana'] ?></p>
+                <p>Horário: <?php echo $aula['hora_inicio'] ?> - <?php echo $aula['hora_fim'] ?></p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php else : ?>
+    <p>Não existem aulas de grupo disponíveis de momento.</p>
+<?php endif; ?>
 </div>
-
 
 <div class="mensagem">
     <p>Inscreve-te já como membro para puderes usufruir destas aulas</p>
