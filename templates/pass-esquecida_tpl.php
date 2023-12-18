418697b509a526a2a0db7a0c36b595169ab3c65a
Patrícia Gomes <patriciagomes@AirdePatricia3.Home>
2023-12-18 13:34:54 +0000

    a

--- templates/pass-esquecida_tpl.php
+++ templates/pass-esquecida_tpl.php
@@ -3,7 +3,7 @@
         <h5>Por favor coloque o seu email para que seja enviado uma senha de recuperação.</h5>
         <form method ="post" action="action_send_pass.php">
           <div class="form-block">
-           <label>Email</label> 
+           <label> Email </label> 
            <input name="email" type="text" required="required"> 
           </div> 
           <button class="filled-button button-submit" type="submit">Enviar senha de recuperação</button>
