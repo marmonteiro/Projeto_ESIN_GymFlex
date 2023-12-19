4fdc5a7aee3e7137a5f5d25545b5fa6691fbfd3f
Patrícia Gomes <patriciagomes@AirdePatricia3.Home>
2023-12-19 22:32:07 +0000

    a

--- templates/aulasgrupo_tpl.php
+++ templates/aulasgrupo_tpl.php
@@ -1,27 +1,27 @@
+<h4> As nossas aulas de grupos são comuns em todos os ginásios! <h4>
 <div class="aulas">
-    <p> <span class=bold> As nossas aulas estão disponivéis em todos os ginásios! </span> </p>
-    <?php if (isset($aulas) && !empty($aulas)) : ?>
-        <?php foreach ($aulas as $aula) : ?>
-            <div class="aula">
-                <div class="image-container">
-                    <img src="<?php echo $aula['imagem_ag'] ?>" alt="<?php echo $aula['nome'] ?>">
-                </div>
-                <div class="info-container">
-                    <h3> <?php echo $aula['nome'] ?></h3>
-                    <p><?php echo $aula['dia_semana'] ?></p>
-                    <p><?php echo $aula['hora_inicio'] ?> - <?php echo $aula['hora_fim'] ?></p>
-                </div>
+<?php if (isset($aulas) && !empty($aulas)) : ?>
+    <?php foreach ($aulas as $aula) : ?>
+        <div class="aula">
+            <div class="image-container">
+                <img src="<?php echo $aula['imagem_ag'] ?>" alt="<?php echo $aula['nome'] ?>">
             </div>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <p>Não existem aulas de grupo disponíveis de momento.</p>
-    <?php endif; ?>
+            <div class="info-container">
+                <h3> <?php echo $aula['nome'] ?></h3>
+                <p><?php echo $aula['dia_semana'] ?></p>
+                <p><?php echo $aula['hora_inicio'] ?> - <?php echo $aula['hora_fim'] ?></p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php else : ?>
+    <p>Não existem aulas de grupo disponíveis de momento.</p>
+<?php endif; ?>
 </div>
 
 <div class = "planos">
     <?php if (!isset($_SESSION['email'])) { ?>
         <a href="registo.php" class="button"> Registe-se </a>
     <?php } else { ?>
-        <a href="inscricao_ag.php" class="button"> Inscreva-se </a>
+        <a href="inscricao_ag" class="button"> Inscreva-se </a>
     <?php } ?>
 </div>
