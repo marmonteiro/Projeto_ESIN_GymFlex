b09beedca9c1d58f0f3b2eb366ee48bdaa4d6e61
ritita02 <rita.marques2002@gmail.com>
2023-12-19 20:22:26 +0000

    css

--- templates/clubes_tpl.php
+++ templates/clubes_tpl.php
@@ -1,8 +1,10 @@
 <div class="clubes">
     <?php foreach ($clubes as $clube): ?>
         <a href='gym_info.php?nome_ginasio=<?= $clube['nome'] ?>'>
+            <img src=imagens/local.png class="normal">
+            <img src=imagens/local_red.png class="red">
             <?= $clube['nome'] ?>
-            <img src='<?= $clube['imagem_url'] ?>' alt='Clube Logo'>
+            <!-- <img src='<?= $clube['imagem_url'] ?>' alt='Clube Logo'> -->
         </a><br>
     <?php endforeach; ?>
 </div>
