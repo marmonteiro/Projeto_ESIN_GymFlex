e334beca1f8e3a1c66639120efd2725e4387dc8a
ritita02 <rita.marques2002@gmail.com>
2023-12-20 16:16:19 +0000

    alteracao passe funcional

--- database/pass.php
+++ database/pass.php
@@ -1,25 +1,11 @@
-<?php 
+<?php
 
-#Buscar o nome da pessoa pelo email
-/* function getNamePessoaByEmail($email){
-    global $dbh;
-    $stmt = $dbh->prepare('SELECT nome FROM Pessoa where email = ?');
-    $stmt->execute(array($email));
-    return $stmt->fetch()['nome'];
-} 
- */
 function updatePassMembrobyEmail($password, $email)
 {
-    global $dbh;
-      $stmtUpdate = $dbh->prepare('UPDATE Membro
-                                  INNER JOIN Pessoa ON Membro.id = Pessoa.id
-                                  SET Membro.pwd = ?
-                                  WHERE Pessoa.email = ?');
-      $stmtUpdate->execute(array(sha1($password), $email));
-    } 
-/* 
-    function updatePassPersonbyEmail($password,$email){       #nao esta a conseguir atualizar na base de dados
-      global $dbh;
-      $stmt = $dbh->prepare('UPDATE Person set password = ? where email = ?');
-      $stmt->execute(array(sha1($password),$email));
-    } */
+  global $dbh;
+  $stmtUpdate = $dbh->prepare('UPDATE Membro
+                                  SET pwd = ?
+                                  WHERE id = (SELECT id FROM Pessoa WHERE email = ?)');
+  $stmtUpdate->execute(array(hash('sha256', $password), $email));
+}
+
